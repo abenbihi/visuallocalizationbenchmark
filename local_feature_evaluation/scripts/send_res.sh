18ae9f171b105fd4c7c0f8b776dd24510150759b
abenbihi <assia.benbihi@gmail.com>
2020-10-16 17:31:50 +0200

    fix result saving script

--- local_feature_evaluation/scripts/send_res.sh
+++ local_feature_evaluation/scripts/send_res.sh
@@ -1,28 +1,33 @@
 #!/bin/sh
 
 data=aachen
-method=ngransac
-trials=0
+method=oanet
+trials=2
 
-remote_dir=benbiass@147.32.84.13:/datagrid/personal/benbiass/ws/tools/vlb/local_feature_evaluation/
+remote_url=benbiass@147.32.84.13
+remote_dir=/datagrid/personal/benbiass/ws/tools/vlb/local_feature_evaluation/
 
 for trial in "$trials"
 do
   res_dir=res/aachen/"$method"/"$trial"/loc
-  #ssh benbiass@147.32.84.13 "cd "$remote_dir"; mkdir -p "$res_dir""
-  #echo "$?"
-  #if [ "$?" -ne 0 ]; then
-  #  echo "Error: failed to create remote directory."
-  #  exit 1
-  #fi
+  ssh benbiass@147.32.84.13 "cd "$remote_dir"; mkdir -p "$res_dir""
+  #echo "ssh benbiass@147.32.84.13 cd "$remote_dir"; echo mkdir -p "$res_dir""
   
-  rsync -avh "$res_dir"/Aachen_eval_ngransac.txt "$remote_dir""$res_dir"
-  rsync -avh res/"$data"/README.md  "$remote_dir"/res/"$data"
-  echo "rsync -avh "$res_dir"/Aachen_eval_ngransac.txt "$remote_dir""$res_dir""
+  echo "$?"
+  if [ "$?" -ne 0 ]; then
+    echo "Error: failed to create remote directory."
+    exit 1
+  fi
+  
+  rsync -avh "$res_dir"/Aachen_eval_"$method".txt "$remote_url":"$remote_dir""$res_dir"
+  rsync -avh res/"$data"/"$method"/README.md  "$remote_url":"$remote_dir"/res/"$data"/"$method"
+  #echo "rsync -avh "$res_dir"/Aachen_eval_ngransac.txt "$remote_url":"$remote_dir""$res_dir""
+  #echo "rsync -avh res/"$data"/README.md  "$remote_url":"$remote_dir"/res/"$data""
+ 
   if [ "$?" -ne 0 ]; then
     echo "Error: failed to send file."
     echo "File to send: "$res_dir"/Aachen_eval_ngransac.txt"
-    echo "Destination: "$remote_dir""$res_dir""
+    echo "Destination: "$remote_url":"$remote_dir""$res_dir""
     exit 1
   fi
 done
