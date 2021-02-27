4af868bd9d6617630acf3db2ee12d40b7093627a
abenbihi <assia.benbihi@gmail.com>
2021-02-27 19:13:28 +0100

    iccv xp ongoing

--- local_feature_evaluation/aachen_box.py
+++ local_feature_evaluation/aachen_box.py
@@ -10,13 +10,9 @@ import subprocess
 
 import sqlite3
 
-import torch
-
 import types
 
 from tqdm import tqdm
-
-from matchers import mutual_nn_matcher
 
 from camera import Camera
 
@@ -24,8 +20,8 @@ from utils import quaternion_to_rotation_matrix, camera_center_to_translation
 
 import sys
 IS_PYTHON3 = sys.version_info[0] >= 3
-    
-DEBUG = (1==1) # run the reconstruction only a subset of images
+
+DEBUG = (1==1)
 
 def array_to_blob(array):
     if IS_PYTHON3:
@@ -45,7 +41,7 @@ def recover_database_images_and_ids(paths, args):
     for row in cursor:
         images[row[0]] = row[1]
         cameras[row[0]] = row[2]
-
+        #print(row[0])
     # Close the connection to the database.
     cursor.close()
     connection.close()
@@ -151,49 +147,39 @@ def import_features(images, paths, args):
     # Import the features.
     print('Importing features...')
     if DEBUG:
-        fns = np.loadtxt("%s/fn.txt"%paths.scene_path, dtype=str)
+        subset_fn = np.unique(np.loadtxt(paths.match_list_path, dtype=str))
     
-    false_count = 0
-    count = 0
-    day_count = 0
+    count, false_count = 0,0
     for image_name, image_id in tqdm(images.items(), total=len(images.items())):
-        if "query/day" in image_name:
-            day_count += 1
-            #exit(0)
+        if DEBUG:
+            if image_name not in subset_fn:
+                continue
+
         features_path = "%s/%s.txt"%(paths.feature_path, image_name)
-        #print(features_path)
 
         if not os.path.exists(features_path):
-            keypoints = np.arange(8).reshape((4,2))
+            #print(features_path)
             false_count += 1
-            # I did not compute features for this img because it is not matched
-            # later i.e. not used
-            #continue
-        # debug (process only a subset of the scene)
-        elif DEBUG and (image_name.split(".")[0] not in fns):
             keypoints = np.arange(8).reshape((4,2)).astype(np.float32)
-            false_count += 1
-            #continue
+            continue
         else:
-            # load features written in colmap format
-            lines = [l.split("\n")[0] for l in open(features_path).readlines()]
-            header = lines[0]
-            features = np.array([l.split(" ") for l in lines[1:]])
-            #print(header)
-            #print(features.shape)
-            keypoints = features[:,:2].astype(np.float32)
-            count += 1
-        
-        # Add placeholder scale, orientation.
+            features = np.loadtxt(features_path, skiprows=1)
+            keypoints = features[:,:4].astype(np.float32)
+        count += 1
+ 
         n_keypoints = keypoints.shape[0]
+        
+        # Keep only x, y coordinates.
+        keypoints = keypoints[:, : 2]
+        # Add placeholder scale, orientation.
         keypoints = np.concatenate([keypoints, np.ones((n_keypoints, 1)), np.zeros((n_keypoints, 1))], axis=1).astype(np.float32)
+       
         keypoints_str = keypoints.tostring()
         cursor.execute("INSERT INTO keypoints(image_id, rows, cols, data) VALUES(?, ?, ?, ?);",
                        (image_id, keypoints.shape[0], keypoints.shape[1], keypoints_str))
         connection.commit()
     
-    print("day_count: %d"%day_count)
-    #print("%d/%d"%(count, false_count))
+    print("%d/%d"%(false_count, count))
     # Close the connection to the database.
     cursor.close()
     connection.close()
@@ -213,36 +199,53 @@ def match_features(images, paths, args):
 
     # Match the features and insert the matches in the database.
     print('Matching...')
-    if DEBUG:
-        subset_matches = [l.split("\n")[0] for l in 
-            open("%s/sorted_pairs.txt"%paths.scene_path).readlines()]
 
     with open(paths.match_list_path, 'r') as f:
         raw_pairs = f.readlines()
     
     image_pair_ids = set()
-    for match_id, raw_pair in enumerate(tqdm(raw_pairs, total=len(raw_pairs))):
+    count, false_count = 0,0
+    for raw_pair in tqdm(raw_pairs, total=len(raw_pairs)):
         image_name1, image_name2 = raw_pair.strip('\n').split(' ')
-        #if "%s %s"%(image_name1.split(".")[0], image_name2.split(".")[0]) not in subset_matches:
-        if (DEBUG) and ("%s %s"%(image_name1, image_name2) not in subset_matches):
+
+        fn1 = image_name1.split(".")[0]
+        fn2 = image_name2.split(".")[0]
+        match_fn = "%s/%s_%s.txt"%(paths.match_path, fn1.replace("/","-"), fn2.replace("/","-"))
+        #break
+
+        if not os.path.exists(match_fn):
+            #print(match_fn)
             matches = np.array([[0,0],[1,1]]).astype(np.uint32)
-        #    print("KO %s %s"%(image_name1.split(".")[0], image_name2.split(".")[0]))
+            false_count += 1
+            #continue
         else:
-            #print("OK %s %s"%(image_name1.split(".")[0], image_name2.split(".")[0]))
-            match_path = "%s/%d.txt"%(args.match_path, match_id)
-            print(match_path)
-            exit(0)
-            matches = np.loadtxt(match_path, dtype=str).reshape((-1,2))
-            #print("%s.jpg"%matches[0,0],image_name1)
-            assert(("%s.jpg"%matches[0,0])==image_name1)
-            assert(("%s.jpg"%matches[0,1])==image_name2)
-            if (matches.shape[0] == 1):
-                # only the first line holding the image names i.e. no matches
-                #print("%d %s %s: no matches"%(match_id, image_name1, image_name2))
-                matches = np.array([[0,0],[1,1]]).astype(np.uint32)
-                #continue
+            #print("match_fn:%s"%match_fn)
+            if args.method_name == "bm":
+                matches = np.loadtxt(match_fn)
+                if matches.shape[0] == 0: # bm could not match keypoints
+                    matches = np.array([[0,0],[1,1]]).astype(np.uint32) # random matches
+                else:
+                    matches = matches[:,:2].astype(np.uint32)
+            elif args.format == "anubis":
+                matches = [l.split("\n")[0].split(" ") for l in
+                        open(match_fn).readlines()]
+                if (len(matches)) == 1:
+                    matches = np.array([[0,0],[1,1]]).astype(np.uint32) # random matches
+                else:
+                    matches = np.array(matches[1:]).astype(np.uint32)
+                    #print(matches)
             else:
-                matches = matches[1:,:].reshape((-1,2)).astype(np.uint32)
+                matches = np.loadtxt(match_fn, dtype=int).astype(np.uint32)
+            matches = matches.reshape((-1,2))
+        count += 1
+
+        
+        #features_path1 = os.path.join(paths.image_path, '%s.%s' % (image_name1, args.method_name))
+        #features_path2 = os.path.join(paths.image_path, '%s.%s' % (image_name2, args.method_name))
+
+        #descriptors1 = torch.from_numpy(np.load(features_path1)['descriptors']).to(device)
+        #descriptors2 = torch.from_numpy(np.load(features_path2)['descriptors']).to(device)
+        #matches = mutual_nn_matcher(descriptors1, descriptors2).astype(np.uint32)
 
         image_id1, image_id2 = images[image_name1], images[image_name2]
         image_pair_id = image_ids_to_pair_id(image_id1, image_id2)
@@ -258,6 +261,7 @@ def match_features(images, paths, args):
                        (image_pair_id, matches.shape[0], matches.shape[1], matches_str))
         connection.commit()
     
+    print("%d/%d"%(false_count,count))
     # Close the connection to the database.
     cursor.close()
     connection.close()
@@ -265,7 +269,6 @@ def match_features(images, paths, args):
 
 def geometric_verification(paths, args):
     print('Running geometric verification...')
-    print(paths.match_list_path)
 
     subprocess.call([os.path.join(args.colmap_path, 'colmap'), 'matches_importer',
                      '--database_path', paths.database_path,
@@ -316,21 +319,15 @@ def recover_query_poses(paths, args):
     
     # Recover query names.
     query_image_list_path = os.path.join(args.dataset_path, 'queries/night_time_queries_with_intrinsics.txt')
+    
     with open(query_image_list_path) as f:
         raw_queries = f.readlines()
+    
     query_names = set()
     for raw_query in raw_queries:
         raw_query = raw_query.strip('\n').split(' ')
         query_name = raw_query[0]
         query_names.add(query_name)
-    query_image_list_path = os.path.join(args.dataset_path, 'queries/day_time_queries_with_intrinsics.txt')
-    with open(query_image_list_path) as f:
-        raw_queries = f.readlines()
-    for raw_query in raw_queries:
-        raw_query = raw_query.strip('\n').split(' ')
-        query_name = raw_query[0]
-        query_names.add(query_name)
-
 
     with open(os.path.join(paths.final_txt_model_path, 'images.txt')) as f:
         raw_extrinsics = f.readlines()
@@ -358,51 +355,52 @@ if __name__ == "__main__":
     parser.add_argument('--res_path', type=str, required=True)
     parser.add_argument('--feat_path', type=str, required=True)
     parser.add_argument('--match_path', type=str, required=True)
-    parser.add_argument("--match_list", type=str, required=True)
-    parser.add_argument('--scene_path', type=str, required=True)
+    parser.add_argument('--format', type=str, required=True)
+ 
     args = parser.parse_args()
 
-    # Torch settings for the matcher.
-    use_cuda = torch.cuda.is_available()
-    device = torch.device("cuda:0" if use_cuda else "cpu")
+    ## Torch settings for the matcher.
+    #use_cuda = torch.cuda.is_available()
+    #device = torch.device("cuda:0" if use_cuda else "cpu")
 
     # Create the extra paths.
     paths = types.SimpleNamespace()
     paths.dummy_database_path = os.path.join(args.dataset_path, 'database.db')
-    paths.reference_model_path = os.path.join(args.dataset_path, '3D-models')
-
-    #paths.match_list_path = os.path.join(args.scene_path, "sorted_pairs.txt")
-    paths.match_list_path = args.match_list #path.join(args.dataset_path, 'image_pairs_to_match.txt')
-    #paths.match_list_path = os.path.join(args.dataset_path, 'image_pairs_to_match_light.txt')
-
+    #paths.database_path = os.path.join(args.dataset_path, args.method_name + '.db')
     paths.image_path = os.path.join(args.dataset_path, 'images', 'images_upright')
+    #paths.features_path = os.path.join(args.dataset_path, args.method_name)
+    paths.reference_model_path = os.path.join(args.dataset_path, '3D-models')
+    paths.match_list_path = os.path.join(args.dataset_path, 'image_pairs_to_match.txt')
+    #paths.match_list_path = os.path.join(args.dataset_path, 'image_pairs_to_match_light1.txt')
+    
+    #paths.empty_model_path = os.path.join(args.dataset_path, 'sparse-%s-empty' % args.method_name)
+    #paths.database_model_path = os.path.join(args.dataset_path, 'sparse-%s-database' % args.method_name)
+    #paths.final_model_path = os.path.join(args.dataset_path, 'sparse-%s-final' % args.method_name)
+    #paths.final_txt_model_path = os.path.join(args.dataset_path, 'sparse-%s-final-txt' % args.method_name)
+    #paths.prediction_path = os.path.join(args.dataset_path, 'Aachen_eval_[%s].txt' % args.method_name)
+    
     paths.feature_path = args.feat_path
     paths.match_path = args.match_path
-    paths.scene_path = args.scene_path
-
     paths.database_path = "%s/database.db"%args.res_path
     paths.empty_model_path = "%s/sparse-%s-empty"%(args.res_path, args.method_name)
     paths.database_model_path = "%s/sparse-%s-database"%(args.res_path, args.method_name)
     paths.final_model_path = "%s/sparse-%s-final"%(args.res_path, args.method_name)
     paths.final_txt_model_path = "%s/sparse-%s-final-txt"%(args.res_path, args.method_name)
     paths.prediction_path = "%s/Aachen_eval_%s.txt"%(args.res_path, args.method_name)
-    
+
+
     # Create a copy of the dummy database.
     if os.path.exists(paths.database_path):
         raise FileExistsError('The database file already exists for method %s.' % args.method_name)
     shutil.copyfile(paths.dummy_database_path, paths.database_path)
     
-    # Reconstruction pipeline.
+    ## Reconstruction pipeline.
     camera_parameters = preprocess_reference_model(paths, args)
     images, cameras = recover_database_images_and_ids(paths, args)
-    #print(images)
-    #exit(0)
     generate_empty_reconstruction(images, cameras, camera_parameters, paths, args)
-    
     import_features(images, paths, args)
     match_features(images, paths, args)
-    
-    #geometric_verification(paths, args)
-    #reconstruct(paths, args)
-    #register_queries(paths, args)
-    #recover_query_poses(paths, args)
+    geometric_verification(paths, args)
+    reconstruct(paths, args)
+    register_queries(paths, args)
+    recover_query_poses(paths, args)
--- local_feature_evaluation/scripts/aachen.sh
+++ local_feature_evaluation/scripts/aachen.sh
@@ -1,96 +1,33 @@
 #!/bin/sh
 . ./scripts/export_path.sh
-#exit 1
 
 data=aachen
 data_dir=data/aachen-day-night/
 colmap_dir="$WS_DIR"tools/colmap/build/src/exe/
 
+method=anubis
+trial=3
 
-#method=elf
-#feat_path="$WS_DIR"/tf/elf/res/aachen/elf/0/
-#loc_trial=0
-#res_path=res/"$data"/"$method"/"$loc_trial"/loc/
-
-#feat_path="$WS_DIR"tools/imb/dream_cpp/res/sift/aachen/features/
-#method=sift
-#loc_trial=0
-
-# box trial: 318
-loc_trial=3
-feat_trial=4
-match_trial=1
-scene_trial=2
-
-# box trial: 342
-loc_trial=4
-feat_trial=4
-match_trial=3
-#scene_trial=4
-scene_trial=2
-
-# draft
-loc_trial=5
+anubis_dir="$WS_DIR"/tools/anubis/res/localization/"$trial"
+feat_path="$anubis_dir"/features/
+match_path="$anubis_dir"/matches/
 
 # input
-feat_path="$WS_DIR"tools/imb/dream_cpp/res/sp/aachen/features/"$feat_trial"/
-match_path="$WS_DIR"tools/imb/dream_cpp/res/sp/aachen/match/"$match_trial"/
-scene_path="$WS_DIR"datasets/pydata/aachen/meta/scenes/"$scene_trial"/
+#feat_path="$WS_DIR"tools/imb/dream_cpp/res/sp/aachen/features/"$feat_trial"/
+#match_path="$WS_DIR"tools/imb/dream_cpp/res/sp/aachen/match/"$match_trial"/
+##scene_path="$WS_DIR"datasets/pydata/aachen/meta/scenes/"$scene_trial"/
 # output
-method=box_sp
-res_path=res/"$data"/"$method"/"$loc_trial"/loc/
-
-
-# yi 2018 learning
-
-
-# oanet
-method=oanet
-feat_path="$WS_DIR"/tools/bm/res/features/0/ # sift (opencv python)
-match_path="$WS_DIR"/tf/OANet/res/0/
-loc_trial=2
-res_path=res/"$data"/"$method"/"$loc_trial"/loc/
-
-# ngransac
-method=ngransac
-feat_path="$WS_DIR"/tools/bm/res/features/2/ # upright root sift (opencv c++)
-match_path="$WS_DIR"/tf/ngransac/res/5/
-loc_trial=0
-res_path=res/"$data"/"$method"/"$loc_trial"/loc/
-
-# bm
-method=bm
-feat_path="$WS_DIR"/tools/bm/res/features/upright-root-sift_cpp_8000/aachen/
-bm_trial=0 # 25 :( # loc_trial 0
-bm_trial=7 # inchAllah
-bm_iter=0
-
-match_path="$WS_DIR"/tools/imb/dream_cpp/res/bm/"$bm_trial"/"$bm_iter"/aachen/kp_match/
-loc_trial=2
-res_path=res/"$data"/"$method"/"$loc_trial"/loc/
-
-#cp -r res/"$data"/"$method"/"$trial"/loc_ref res/"$data"/"$method"/"$trial"/loc/
+#method=box_sp
+res_path=res/"$data"/"$method"/"$trial"
 
 rm -rf "$res_path"
 mkdir -p "$res_path"
 
-#python3 reconstruction_pipeline.py \
-python3 reconstruction_pipeline_custom_matches.py \
+python3 aachen_box.py \
   --dataset_path "$data_dir" \
   --colmap_path "$colmap_dir" \
   --method_name "$method" \
   --res_path "$res_path" \
   --feat_path "$feat_path" \
-  --match_path "$match_path"
-
-#python3 aachen_box.py \
-#  --dataset_path "$data_dir" \
-#  --colmap_path "$colmap_dir" \
-#  --method_name box_sp \
-#  --scene_path "$scene_path" \
-#  --feat_path "$feat_path" \
-#  --match_path "$match_path" \
-#  --match_list data/aachen-day-night/image_pairs_to_match_gt_night_gem_day.txt \
-#  --res_path "$res_path"
-
-  #--match_list data/aachen-day-night/image_pairs_to_match.txt \ gt night
+  --match_path "$match_path" \
+  --format "$method"
