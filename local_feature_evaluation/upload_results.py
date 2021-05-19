3357d2f0dcca5c9291d30352e8bcb1276890ec35
abenbihi <assia.benbihi@gmail.com>
2021-05-19 15:35:42 +0200

    python script to upload results ok

---
+++ local_feature_evaluation/upload_results.py
@@ -0,0 +1,39 @@
+"""
+Author: Tomas Jenicek
+"""
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+session = "4d3btcg5rfkjv0pn6mk7j4jml473zz9r"
+
+resp = requests.get("https://www.visuallocalization.net/submission/",
+        headers={"Cookie": f"sessionid={session}"})
+
+bs = BeautifulSoup(resp.content, features="html.parser")
+csrf = bs.select('form > input[name="csrfmiddlewaretoken"]')[0].attrs['value']
+
+url = "https://www.visuallocalization.net/submission/"
+
+headers = {
+        "Cookie": f"csrftoken={resp.cookies['csrftoken']}; sessionid={session}",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+}
+
+data = {
+        "csrfmiddlewaretoken": csrf,
+        "method_name": "test",
+        "publication_url": "",
+        "code_url": "",
+        "info_field": "",
+        "dataset": "aachen",
+        "workshop_submission": "default",
+}
+
+files = {
+        "result_file": open("/home/abenbihi/ws/tools/vlb/local_feature_evaluation/res/aachen/anubis/38/Aachen_eval_anubis.txt", "r"),
+}
+
+resp = requests.post(url, files=files, data=data, headers=headers)
+
+print(resp.status_code)
