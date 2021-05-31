92dda714ac25202a97a304188697bb8d588882b6
abenbihi <assia.benbihi@gmail.com>
2021-05-31 15:38:39 +0200

    result save/gather scripts ok

---
+++ local_feature_evaluation/scripts/gather_cmu_res.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+method=sift
+
+match_trial=9
+match_iter=0
+loc_iter=0
+
+res_path=res/cmu/sift/"$match_trial"/"$match_iter"/"$loc_iter"/
+
+global_res_path="$res_path"/CMU_eval_"$method".txt
+echo "" > "$global_res_path"
+
+for slice_id in 2
+do
+  slice_res_path="$res_path"/CMU_eval_"$method"_slice"$slice_id".txt
+  echo "" > "$slice_res_path"
+  for cam_id in 0 #1
+  do
+    slice_cam_res_path="$res_path"/CMU_eval_"$method"_slice"$slice_id"_c"$cam_id".txt
+    echo "" > "$slice_cam_res_path"
+    for survey_id in 0 1 #2 3 4 5 6 7 8 9 10
+    do
+      eval_fn="$res_path"/"$slice_id"_c"$cam_id"_"$survey_id"/Aachen_eval_sift.txt
+
+      cat "$eval_fn" >> "$global_res_path"
+      cat "$eval_fn" >> "$slice_res_path"
+      cat "$eval_fn" >> "$slice_cam_res_path"
+    done
+  done
+done
---
+++ local_feature_evaluation/scripts/send_res_cmu.sh
@@ -0,0 +1,100 @@
+#!/bin/sh
+
+data=cmu
+
+server=rci
+server=ritz
+#server=lascar
+
+# set server path
+if [ "$server" = rci ]; then
+  address=benbiass@login.rci.cvut.cz
+  remote_ws=/home/benbiass/ws/
+elif [ "$server" = ritz ]; then
+  address=benbiass@147.32.84.13
+  remote_ws=/mnt/datagrid/personal/benbiass/ws/
+elif [ "$server" = lascar ]; then
+  #address=benbiass@192.168.85.150
+  address=benbiass@lcraid.felk.cvut.cz
+  remote_ws=/mnt/ssd/temporary/benbiass/ws/
+else
+  echo "Error: server not specified."
+  exit 1
+fi
+dst="$address":"$remote_ws"
+echo "dst: "$dst""
+
+remote_vlb_dir="$remote_ws"/tools/vlb/local_feature_evaluation/
+
+method=adalam
+match_trial=0
+
+method=anubis
+match_trial=55
+
+method=sift
+match_trial=9
+
+loc_iter_max=1
+match_iter_max=1
+
+match_iter=0
+while [ "$match_iter" -lt "$match_iter_max" ];
+do
+  loc_iter=0
+  while [ "$loc_iter" -lt "$loc_iter_max" ];
+  do
+    res_path=res/"$data"/"$method"/"$match_trial"/"$match_iter"/"$loc_iter"/
+
+    for slice_id in 2
+    do
+      for cam_id in 0 #1
+      do
+        for survey_id in 0 #1 2 3 4 5 6 7 8 9 10
+        do
+
+          suffix="$slice_id"_c"$cam_id"_"$survey_id"
+          sub_res_path="$res_path"/"$suffix"/
+
+          ssh "$address" "cd "$remote_vlb_dir"; mkdir -p "$sub_res_path""
+          if [ "$?" -ne 0 ]; then
+            echo "Error: failed to create remote directory."
+            exit 1
+          fi
+
+          rsync -avh "$sub_res_path"/Aachen_eval_"$method"*.txt \
+            "$address":"$remote_vlb_dir""$sub_res_path"
+          if [ "$?" -ne 0 ]; then
+            echo "Error: failed to send file."
+            echo "File to send: "$res_dir"/Aachen_eval_"$method".txt"
+            echo "Destination: "$remote_vlb_dir""$sub_res_path""
+            exit 1
+          fi
+          loc_iter="$((loc_iter+1))"
+        done
+        match_iter="$((match_iter+1))"
+      done
+    done
+  done
+done
+
+#for trial in "$trials"
+#do
+#  res_dir=res/aachen/"$method"/"$trial"/
+#  #ssh benbiass@147.32.84.13 "cd "$remote_dir"; mkdir -p "$res_dir""
+#  #echo "$?"
+#  #if [ "$?" -ne 0 ]; then
+#  #  echo "Error: failed to create remote directory."
+#  #  exit 1
+#  #fi
+#  
+#  rsync -avh "$res_dir"/Aachen_eval_"$method".txt "$remote_dir""$res_dir"
+#  #rsync -avh res/"$data"/README.md  "$remote_dir"/res/"$data"
+#  echo "rsync -avh "$res_dir"/Aachen_eval_ngransac.txt "$remote_dir""$res_dir""
+#  if [ "$?" -ne 0 ]; then
+#    echo "Error: failed to send file."
+#    echo "File to send: "$res_dir"/Aachen_eval_ngransac.txt"
+#    echo "Destination: "$remote_dir""$res_dir""
+#    exit 1
+#  fi
+#done
